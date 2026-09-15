92c1b15b41fd909b106c62af93d8648e055d46eb
Christever <christian@steverlynck.fr>
2026-09-15 20:49:33 +0200

    maj

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,9 @@
+import { url } from "@/utils/url";
+
+export const routes = {
+  accueil: url(""),
+  entreprise: url("entreprise"),
+  prestations: url("prestations"),
+  realisations: url("realisations"),
+  contact: url("contact"),
+};

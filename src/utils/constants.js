a3c611b655d2e5e305c49d4eb10e5563a74e1e6d
Christever <christian@steverlynck.fr>
2026-09-16 11:33:43 +0200

    rajout contants et maj footer

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const constants = {
+    title: "Artisan Peintre ",
+    ville: " à Bédarieux et ses alentours ",
+    description: "Peinture intérieure et extérieure, pose de papier peint  pour vos projets de rénovation."
+}
